b4c3e09332a45ea141d0786dd41774fc0292b27e
akio0911 <akio0911@gmail.com>
2021-06-15 17:25:56 +0900

    feedback

--- kadai5/ViewController.swift
+++ kadai5/ViewController.swift
@@ -9,11 +9,11 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    @IBOutlet  private weak var textFiled1: UITextField!
-    @IBOutlet  private weak var textFiled2: UITextField!
+    @IBOutlet private weak var textFiled1: UITextField!
+    @IBOutlet private weak var textFiled2: UITextField!
     
-    @IBOutlet  private weak var resultLabel: UILabel!
-    private var resultNum = 0.0
+    @IBOutlet private weak var resultLabel: UILabel!
+
     //アラートを作る
     private func showAlert(message: String) {
         let alert = UIAlertController(title: "課題", message: message, preferredStyle: .alert)
@@ -21,23 +21,26 @@ class ViewController: UIViewController {
         present(alert, animated: true, completion: nil)
     }
     
-    @IBAction  private func calculateButton(_ sender: Any) {
+    @IBAction private func calculateButton(_ sender: Any) {
         guard let text1 = textFiled1.text else { return }
         guard let text2 = textFiled2.text else { return }
         
-        guard let leftNum = Double(text1)else {
+        guard let leftNum = Double(text1) else {
             showAlert(message: "割られる数を入力してください")
             return
         }
+
         guard let rightNum = Double(text2) else {
             showAlert(message: "割る数を入力してください")
             return
         }
+
         guard rightNum != 0 else {
             showAlert(message: "割る数には０を入力しないで下さい！")
             return
         }
-        resultNum = leftNum / rightNum
+
+        let resultNum = leftNum / rightNum
         resultLabel.text = String(resultNum)
     }
 }
